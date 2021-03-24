cc16f9f65f939c3301ae58edef497d6ce2ad9fd2
AlaaEzzeldin <ala.ezzeldin@gmail.com>
2021-03-24 17:06:11 +0100

    React Redux Forms Revisited--add safe data feature

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,7 +3,7 @@ import {
     Breadcrumb, BreadcrumbItem,
     Button, Row, Col, Label
 } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
 
@@ -28,6 +28,7 @@ class Contact extends Component {
     handleSubmit(values) {
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetFeedbackForm();
         // event.preventDefault();
     }
 
@@ -77,7 +78,7 @@ class Contact extends Component {
                     </div>
                     <div className="col-12 col-md-9">
 
-                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -201,7 +202,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                 </div>
             </div>
